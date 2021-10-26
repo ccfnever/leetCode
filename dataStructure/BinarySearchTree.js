e0b4a2608735d02206c16ab0451f352cb4538628
ccfnever <349090627@qq.com>
2021-10-26 23:04:58 +0800

    BinarySearchTree

--- dataStructure/BinarySearchTree.js
+++ dataStructure/BinarySearchTree.js
@@ -1,4 +1,4 @@
-// 二叉树
+// 搜索二叉树
 class Node {
   constructor(val) {
     this.val = val
@@ -30,12 +30,45 @@ class BinarySearchTree {
     return node
   }
 
+  // 中序遍历，即以小到大的顺序遍历所有节点
+  inOrderTraverse(node, callBack) {
+    if (!node) return false
+    this.inOrderTraverse(node.left, callBack)
+    callBack(node.val)
+    this.inOrderTraverse(node.right, callBack)
+  }
+
+  // 前序遍历
+  preOrderTraverseNode(node, callBack) {
+    if (!node) return false
+    callBack(node.val)
+    this.preOrderTraverseNode(node.left, callBack)
+    this.preOrderTraverseNode(node.right, callBack)
+  }
+
+  // 后序遍历
+  postOrderTraverseNode(node, callBack) {
+    if (!node) return false
+    callBack(node.val)
+    this.postOrderTraverseNode(node.left, callBack)
+    this.postOrderTraverseNode(node.right, callBack)
+  }
+
 }
 
 let tree = new BinarySearchTree(10)
-tree.insert(10)
+tree.insert(3)
 tree.insert(5)
-tree.insert(2)
+tree.insert(6)
+tree.insert(10)
+tree.insert(11)
+tree.insert(12)
+tree.insert(13)
+tree.insert(7)
+tree.insert(8)
+tree.insert(9)
 
-console.log(JSON.stringify(tree.root, null, 4))
+
+console.log(tree.inOrderTraverse(tree.root,(val)=>{console.log(val)}))
+// console.log(JSON.stringify(tree.root, null, 4))
 
